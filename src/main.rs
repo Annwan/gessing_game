c59bccec6a4b766e85b19e23a2b2e198dbddcb97
Annwan <anwankong@gmail.com>
2020-02-05 10:02:24 +0100

    .

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
+
 fn main() {
-    println!("Hello, world!");
+  println!("Gess the number!");
+
+  let secret_nb = rand::thread_rng().gen_range(1,101);
+
+  println!("The secret number is {}", secret_nb);
+
+  let mut guess = String::new();
+
+  io::stdin().read_line(&mut guess)
+    .expect("Failed to read line!");
+  
+  println!("You gessed : {}", guess);
+
+  match guess.cmp(&secret_nb) {
+    Ordering::Less => println!("Too Smalli"),
+  }
 }
